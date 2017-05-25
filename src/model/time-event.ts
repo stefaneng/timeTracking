fc431366d1e8ebfb42889663291721178f1e5d02
Stefan Eng <stefaneng13@gmail.com>
2017-05-25 16:40:56 -0700

    Events displayed with start/stop time

---
+++ src/model/time-event.ts
@@ -0,0 +1,6 @@
+// Represents a timed event
+// For now just start and stop time
+export class TimeEvent {
+  startTime: Date;
+  endTime: Date;
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { TimeEvent } from '../../model/time-event'
 
 @Component({
   selector: 'page-home',
@@ -9,10 +10,23 @@ export class HomePage {
 
   isRunning = false;
 
+  events: Array<TimeEvent> = [];
+  startTime: Date;
+
   constructor(public navCtrl: NavController) {}
 
-  buttonPressed(event) {
-    this.isRunning = ! this.isRunning;
+  startPressed(event) {
+    this.isRunning = true;
+    this.startTime = new Date(Date.now());
   }
 
+  stopPressed(event) {
+    this.isRunning = false;
+    let endTime = new Date(Date.now());
+    this.events.push({
+      startTime: this.startTime,
+      endTime: endTime
+    });
+    this.startTime = null;
+  }
 }
